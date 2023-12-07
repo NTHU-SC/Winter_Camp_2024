f850c8001272d2e374c9e5fa4a0df3b59a2eef60
William-Mou <william.mou1024@gmail.com>
2023-12-07 22:04:05 +0800

    Updated 2024 sponsor

--- src/components/Brands.jsx
+++ src/components/Brands.jsx
@@ -63,7 +63,7 @@ export const Brands = () => (
           </div>
         </div>
       </div>
-      <div className="container px-4 mx-auto 2xl:w-[1200px] xl:w-[1100px] lg:w-[1000px] md:w-4/5 pt-20">
+      {/* <div className="container px-4 mx-auto 2xl:w-[1200px] xl:w-[1100px] lg:w-[1000px] md:w-4/5 pt-20">
         <div className="flex lg:flex-row flex-col items-center -mx-4 justify-center lg:text-left text-center">
           <div className="w-full lg:w-1/2 px-4 mb-12 lg:mb-0">
             <div className="flex flex-col">
@@ -74,7 +74,7 @@ export const Brands = () => (
           </div>
           <div className="w-2/3 sm:w-[620px] lg:w-1/2 mx-auto lg:mx-0 lg:pl-10">
             <div className="flex flex-wrap -m-4">
-              {/* <div className="flex py-3 justify-center">
+              { <div className="flex py-3 justify-center">
                 <a href="https://www.nvidia.com/" target="_blank" rel="noopener noreferrer">
                   <img
                     src={NVIDIA_logo}
@@ -82,8 +82,8 @@ export const Brands = () => (
                     className="rounded-xl  custom-border-gray"
                   />
                 </a>
-              </div> */}
-              {/* <div className="flex py-3 justify-center">
+              </div> }
+              { <div className="flex py-3 justify-center">
                 <a href="https://www.aims.org.tw/" target="_blank" rel="noopener noreferrer">
                   <img
                     src={AiMS_logo}
@@ -91,11 +91,11 @@ export const Brands = () => (
                     className="rounded-xl  custom-border-gray"
                   />
                 </a>
-              </div> */}
+              </div> }
             </div>
           </div>
         </div>
-      </div>
+      </div> */}
     </motion.div>
   </section>
 );
