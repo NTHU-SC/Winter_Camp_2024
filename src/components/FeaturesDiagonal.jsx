a24b45e788c74951513549dbd4daadc231f8bef9
William-Mou <william.mou1024@gmail.com>
2023-12-09 00:52:16 +0800

    Updated course for 2024

--- src/components/FeaturesDiagonal.jsx
+++ src/components/FeaturesDiagonal.jsx
@@ -39,12 +39,12 @@ export const FeaturesDiagonal = () => {
               精挑細選
             </span> */}
             <h2 className="mt-10 mb-8 text-4xl lg:text-5xl custom-block-big-title">
-              專業知識密集課程  
+              專業知識密集課程
             </h2>
-            <h2 className="mt-10 mb-8 text-4xl lg:text-5xl custom-block-big-title">
-              To Be announced 
-            </h2>
-            
+            {/* <h2 className="mt-10 mb-8 text-4xl lg:text-5xl custom-block-big-title">
+              To Be announced
+            </h2> */}
+
             {/* <p className="mb-16 text-customGrayText leading-loose">
               Our platform enables you to launch your data-driven projects with
               ease. Boost productivity and achieve better results. Empower your
@@ -92,82 +92,67 @@ export const FeaturesDiagonal = () => {
 
 const MyTable = () => {
   return (
-
-    
     <table className="GeneratedTable">
-      {/* <thead>
+      <thead>
         <tr>
           <th> </th>
-          <th colSpan="2">Day1 8/6 (日)</th>
-          <th>Day2 8/7 (一)</th>
-          <th>Day3 8/8 (二)</th>
-          <th>Day4 8/9 (三)</th>
-          <th>Day5 8/10 (四)</th>
+          <th>第一天 1/29 (一)</th>
+          <th>第二天 1/30 (二)</th>
+          <th>第三天 1/31 (二)</th>
         </tr>
       </thead>
       <tbody>
         <tr>
           <td>9:00~9:30</td>
-          <td colSpan="2">報到</td>
-          <td rowSpan="3">登峰造極<br />(超級電腦簡介與實操)</td>
-          <td rowSpan="3">拜師學藝<br />(學生叢集競賽座談會)</td>
-          <td rowSpan="4">學以致用<br />(高效能計算在科學計算與人工智慧的應用簡介與實操)</td>
-          <td rowSpan="4">增廣見聞<br />(國網中心參訪)</td>
+          <td>報到</td>
+          <td rowSpan="2">學生叢集競賽座談</td>
+          <td rowSpan="3">國網參訪 <br></br>(大型語言模型專題演講)</td>
         </tr>
         <tr>
           <td>9:30~10:00</td>
-          <td colSpan="2">啟航 (開幕)</td>
+          <td>開幕</td>
         </tr>
         <tr>
-          <td>10:00~10:30</td>
-          <td colSpan="2" rowSpan="2">進入平行世界<br />(平行計算簡介 I)</td>
-        </tr>
-        <tr>
-          <td>10:30~12:00</td>
-          <td>NVIDIA 神秘嘉賓</td>
-          <td>超級比一比 (效能競賽)</td>
+          <td>10:00~12:00</td>
+          <td>高效能計算簡介</td>
+          <td>超級電腦簡介實操</td>
         </tr>
         <tr>
           <td>12:00~13:30</td>
-          <td colSpan="2">午餐</td>
-          <td>午餐</td>
           <td>午餐</td>
           <td>午餐</td>
           <td>午餐</td>
         </tr>
         <tr>
-          <td>13:30~15:00</td>
-          <td>追本溯源<br />(Linux教學)</td>
-          <td>探索未來<br />(平行計算簡介 II)</td>
-          <td rowSpan="3">多重宇宙<br />(GPU平行程式教學)</td>
-          <td rowSpan="3">駭客任務:首部曲<br />(程式競賽)</td>
-          <td rowSpan="3">駭客任務:最終戰役<br />(應用競賽)</td>
-          <td>開花結果<br />(競賽成果發表)</td>
+          <td>13:30~14:30</td>
+          <td rowSpan="3">Linux簡介實操</td>
+          <td rowSpan="4">平行程式教學</td>
+          <td>計算效能量測分析</td>
         </tr>
         <tr>
-          <td>15:00~16:00</td>
-          <td colSpan="2" rowSpan="2">一個頭兩個大<br />(MPI平行程式教學)</td>
-          <td>滿載而歸<br />(頒獎、閉幕)</td>
+          <td>14:30~15:30</td>
+          <td>系所介紹</td>
         </tr>
         <tr>
-          <td>15:00~17:30</td>
-          <td rowSpan="3"> <br /><br /></td>
+          <td>15:30~16:30</td>
+          <td>頒發證書、歸賦</td>
+        </tr>
+        <tr>
+          <td>16:30~17:30</td>
+          <td>校園參訪</td>
+          <td rowSpan="3"></td>
         </tr>
         <tr>
           <td>17:30~19:00</td>
-          <td colSpan="2">晚餐，相見歡</td>
-          <td>晚餐</td>
           <td>晚餐</td>
           <td>晚餐</td>
         </tr>
         <tr>
           <td>19:00~21:30</td>
-          <td colSpan="2">手腦並用<br />(上機練習)</td>
-          <td>七手八腳<br />(上機練習)</td>
-          <td>駭客任務:重裝上陣<br />(程式競賽)</td>
-          <td>集思廣益<br />(競賽成果彙整)</td>
+          <td>Linux 實戰練習</td>
+          <td>平行程式實戰練習</td>
         </tr>
-      </tbody> */}
+      </tbody>
     </table>
   );
 };
